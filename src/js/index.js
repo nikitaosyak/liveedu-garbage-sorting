d10a97dacc78226755030ef41e2d6384cd3e24c4
Nikita Osyak <desperatebassplayer@gmail.com>
2018-09-30 13:47:10 +0300

    session 1.3

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ window.onload = () => {
 
     const resources = window.resources = Resources()
 
+    const startGame = () => {
+
+    }
+
     resources.add('digest', 'assets/digest.json').load().then(() => {
         console.log('digest loaded')
 
@@ -14,7 +18,22 @@ window.onload = () => {
 
         resources.load().then(() => {
             console.log('Resources loaded')
-            console.log(resources.raw)
+
+            WebFont.load({
+                google: {
+                    families: ['Crimson Text']
+                },
+                loading: () => console.log('font started loading'),
+                inactive: () => {
+                    console.log('failed to load webfont')
+                    startGame()
+                },
+                active: () => {
+                    console.log('font loading was a success')
+                    startGame()
+                }
+            })
+            // console.log(resources.raw)
         })
     })
 }
